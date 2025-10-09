3065d94e9d32ddd2e627ecf432457252086010f9
Maruchan <mariannai99@icloud.com>
2025-10-09 12:12:52 -0300

    Agregué la funcionalidad para listar productos en la API usando DTO y manejo de errores

--- api-articulos/Controllers/ArticulosController.cs
+++ api-articulos/Controllers/ArticulosController.cs
@@ -6,16 +6,40 @@ using System.Net.Http;
 using System.Web.Http;
 using dominio;
 using negocio;
+using api_articulos.Models;
 
 namespace api_articulos.Controllers
 {
     public class ArticulosController : ApiController
     {
         // GET: api/Articulos
-        public IEnumerable<Articulo> Get()
+        public IEnumerable<ArticuloDTO> Get()
         {
-            ArticuloNegocio negocio = new ArticuloNegocio();
-            return negocio.listar();
+            try
+            {
+                ArticuloNegocio negocio = new ArticuloNegocio();
+                List<Articulo> listaArticulos = negocio.listar();
+
+                List<ArticuloDTO> listaDTO = listaArticulos.Select(a => new ArticuloDTO
+                {
+                    Id = a.IdArticulo,
+                    Nombre = a.Nombre,
+                    Codigo = a.Codigo,
+                    Descripcion = a.Descripcion,
+                    Marca = a.Marca != null ? a.Marca.Descripcion : "",
+                    Categoria = a.Categoria != null ? a.Categoria.Descripcion : "",
+                    Precio = a.Precio,
+                    Imagenes = a.Imagenes != null ? a.Imagenes.Select(img => img.urlImagen).ToList() : new List<string>()
+                }).ToList();
+
+                return listaDTO;
+            }
+            catch (Exception ex)
+            {
+                throw new HttpResponseException(
+                    System.Net.HttpStatusCode.InternalServerError
+                );
+            }
 
         }
         // GET: api/Articulos/5
