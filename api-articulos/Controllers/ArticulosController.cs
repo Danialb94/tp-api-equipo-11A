12916307bc108dc37eb1a36e71cf668020a14723
Maruchan <mariannai99@icloud.com>
2025-10-10 17:37:45 -0300

    Agregué la funcionalidad para dar de alta un artículo con DTO, validando marca y categoría y guardando sus imágenes.

--- api-articulos/Controllers/ArticulosController.cs
+++ api-articulos/Controllers/ArticulosController.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using api_articulos.Models;
+using dominio;
+using negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Web.Http;
-using dominio;
-using negocio;
-using api_articulos.Models;
 
 namespace api_articulos.Controllers
 {
@@ -49,8 +50,83 @@ namespace api_articulos.Controllers
         }
 
         // POST: api/Articulos
-        public void Post([FromBody]string value)
+        public IHttpActionResult Post([FromBody] ArticuloDTO nuevoArticulo)
         {
+            try
+            {
+                // Valido que el cliente me haya mandado datos.
+                // Si el objeto "nuevoArticulo" está vacío (null),
+                // devuelvo un mensaje de error al cliente.
+                if (nuevoArticulo == null)
+                {
+                    return BadRequest("No se enviaron datos del producto.");
+                }
+
+                // Creo un objeto del dominio(Articulo)
+                Articulo articulo = new Articulo();
+                articulo.Codigo = nuevoArticulo.Codigo;
+                articulo.Nombre = nuevoArticulo.Nombre;
+                articulo.Descripcion = nuevoArticulo.Descripcion;
+                articulo.Precio = nuevoArticulo.Precio;
+
+                //Obtengo el Id de la Marca desde la base de datos usando su descripción
+                AccesoDatos datos = new AccesoDatos();
+                datos.setearConsulta("SELECT Id FROM MARCAS WHERE Descripcion = @descripcion");
+                datos.setearParametro("@descripcion", nuevoArticulo.Marca);
+                datos.ejecutarLectura();
+
+                if (datos.Lector.Read())
+                {
+                    articulo.Marca = new Marca();
+                    articulo.Marca.IdMarca = (int)datos.Lector["Id"];
+                    articulo.Marca.Descripcion = nuevoArticulo.Marca;
+                }
+                else
+                {
+                    return BadRequest("La marca indicada no existe en la base de datos. ");
+                }
+                datos.cerrarConexion();
+
+                //Hago lo mismo con la Categoría
+                datos = new AccesoDatos();
+                datos.setearConsulta("SELECT Id FROM CATEGORIAS WHERE Descripcion = @descripcion");
+                datos.setearParametro("@descripcion", nuevoArticulo.Categoria);
+                datos.ejecutarLectura();
+
+                if (datos.Lector.Read())
+                {
+                    articulo.Categoria = new Categoria();
+                    articulo.Categoria.IDCategoria = (int)datos.Lector["Id"];
+                    articulo.Categoria.Descripcion = nuevoArticulo.Categoria;
+                }
+                else
+                {
+                    return BadRequest("La categoría indicada no existe en la base de datos.");
+                }
+                datos.cerrarConexion();
+
+                //Creo la lista de imágenes
+                List<Imagen> listaImg = new List<Imagen>();
+                if (nuevoArticulo.Imagenes != null)
+                {
+                    foreach (var url in nuevoArticulo.Imagenes)
+                    {
+                        Imagen img = new Imagen();
+                        img.urlImagen = url;
+                        listaImg.Add(img);
+                    }
+                }
+
+                ArticuloNegocio negocio = new ArticuloNegocio();
+                negocio.agregar(articulo, listaImg);
+
+                return Ok("Producto agregado correctamente.");
+            }
+            catch (Exception ex)
+            {
+
+                return InternalServerError(ex);
+            }
         }
 
         // PUT: api/Articulos/5
